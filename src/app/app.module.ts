817093457cce0815604490281fa759f1105c0a65
ShubhangiD21 <shubadongare@gmail.com>
2021-08-04 17:55:05 +0530

    changes in file name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 import { AmexioWidgetModule } from "amexio-ng-extensions"; // Import Amexio library
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { EventBaseComponent } from './event.base/event.base.component';
-import { ValueAccessorBaseComponent } from './value-accesor/value-accesor.component';
-import { AmexioDropdownComponent } from './amexio-dropdown1/amexio-dropdown1.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { EventBaseComponent } from "./event.base/event.base.component";
+import { ValueAccessorBaseComponent } from "./value-accesor/value-accesor.component";
+import { DropdownComponent } from './dropdown/dropdown.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AmexioDropdownComponent,
-     EventBaseComponent,
-     ValueAccessorBaseComponent,
-   
+    EventBaseComponent,
+    ValueAccessorBaseComponent,
+    DropdownComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    AmexioWidgetModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, AmexioWidgetModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/dropdown/dropdown.component.spec.ts
+++ src/app/dropdown/dropdown.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AmexioDropdown1Component } from './amexio-dropdown1.component';
+import { DropdownComponent } from './dropdown.component';
 
-describe('AmexioDropdown1Component', () => {
-  let component: AmexioDropdown1Component;
-  let fixture: ComponentFixture<AmexioDropdown1Component>;
+describe('DropdownComponent', () => {
+  let component: DropdownComponent;
+  let fixture: ComponentFixture<DropdownComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AmexioDropdown1Component ]
+      declarations: [ DropdownComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AmexioDropdown1Component);
+    fixture = TestBed.createComponent(DropdownComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
